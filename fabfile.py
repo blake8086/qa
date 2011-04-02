e95062b578a6a09fa3b677de51b1b9477e5d54fd
Blake Householder <blake8086@gmail.com>
2011-04-02 16:39:26 -0700

    added fabric deployment file

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from __future__ import with_statement
+from fabric.api import abort, cd, env, local, run, settings
+from fabric.contrib.console import confirm
+
+env.hosts = ['blake8086@blake8086.webfactional.com']
+
+def development():
+	with cd('/home/blake8086/webapps/c4c_development/qa/'):
+		run('git pull')
+	run('/home/blake8086/webapps/c4c_development/apache2/bin/stop')
+	#wait till it's down
+	
+	run('/home/blake8086/webapps/c4c_development/apache2/bin/start')
+	#wait till it's up
