14d6974720c76561bfa66487d5289b5594e8d3b2
Blake Householder <blake8086@gmail.com>
2011-04-05 00:39:23 -0500

    added development boto dir to views.py

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append('/home/blake8086/webapps/c4c_development/qa')
 from qa.boto.fps.connection import FPSConnection
 from django import forms
 from django.contrib import messages
