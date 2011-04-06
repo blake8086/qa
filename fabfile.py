1a8be2e612d98211431815b2ebbd14eb905f57c1
Blake Householder <blake8086@gmail.com>
2011-04-05 22:31:38 -0700

    simplified fabfile

--- fabfile.py
+++ fabfile.py
@@ -7,10 +7,7 @@ env.hosts = ['blake8086@blake8086.webfactional.com']
 def development():
 	with cd('/home/blake8086/webapps/c4c_development/qa/'):
 		run('git pull')
-	run('/home/blake8086/webapps/c4c_development/apache2/bin/stop')
-	#wait till it's down
-	run('/home/blake8086/webapps/c4c_development/apache2/bin/start')
-	#wait till it's up
+	run('/home/blake8086/webapps/c4c_development/apache2/bin/restart')
 
 def static():
 	local('cp -r /Users/blake8086/Documents/qa/static/* /Library/WebServer/Documents/')
